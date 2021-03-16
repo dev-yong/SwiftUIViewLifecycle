c0a7c1af0558a1c46ad0b1e6f29e8129a275657a
dev-yong <rhkdrmfh@gmail.com>
2021-03-16 23:26:29 +0900

    Initial Commit

---
+++ SwiftUIViewLifecycle/SwiftUIViewLifecycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewLifecycleApp.swift
+//  SwiftUIViewLifecycle
+//
+//  Created by 이광용 on 2021/03/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewLifecycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
